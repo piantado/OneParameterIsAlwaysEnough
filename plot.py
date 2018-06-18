0980ea6586d6b3f97f90beef0e89794d6fd06002
Steven Piantadosi <spiantado@gmail.com>
2018-06-18 15:50:21 -0400

    fixed indents, added back in line to append points

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ from random import sample, random
 from PIL import Image
 
 # Read an image
-im = Image.open('Miro.png')
+im = Image.open('miro.png')
 
 Nx = 750 # How m any x-locations do we sample?
 
@@ -19,23 +19,23 @@ for x in xrange(Nx):
 
     px = int((float(x)/float(Nx)) * im.width) #NOTE: im.width may depend on the version of PIL?
 
-	# find all of the y values that are black (or alomst black) at location px
+    # find all of the y values that are black (or alomst black) at location px
     on = []
     for py in xrange(1,im.height-1):
         pixel = im.getpixel( (px,py) )
         if(pixel[-1] == 0): continue # transparent
         if all(z < 10 for z in pixel[:3]):
             on.append((im.height-float(py))/float(im.height))
-    
-    # fix missing with zero -- if there are no y values that are black, we pretend there was one at 0.01
-    if(len(on) == 0): 
-        on.append( 0.01 )
-    
-	# choose which y value we sample
-    yy = sample(on,1)[0] 
-    
-    # append it to our list of points, scaling y (see paper) to be in (0,.5)
 
+        # fix missing with zero -- if there are no y values that are black, we pretend there was one at 0.01
+        if(len(on) == 0):
+            on.append( 0.01 )
+
+        # choose which y value we sample
+        yy = sample(on,1)[0]
+
+        # append it to our list of points, scaling y (see paper) to be in (0,.5)
+        points.append( (px, yy) )
 
 # ------------------------------------------------------
 # Define some useful functions, importing from mpmath
@@ -45,12 +45,12 @@ import mpmath
 from mpmath import mp # mpmath allows arbitrary precision floating points
 
 # set the precision required
-mp.dps=2000 
+mp.dps=2000
 
 def float2binary(x):
     # convert x to a binary string
     assert 0 < x < 1
-    
+
     outstr = ""
     while x>0:
         if x < 0.5:
@@ -67,7 +67,7 @@ def binary2float(s):
     for i, b in enumerate(s):
         if b == '1':
             v += mpmath.power(2,-(i+1))
-    return v 
+    return v
 
 def phi(z):
     return mpmath.power(mpmath.sin(z * mpmath.pi * 2.0), 2.0)
@@ -81,14 +81,14 @@ def m(rx, theta):
 # ------------------------------------------------------
 # Now the actual code
 # ------------------------------------------------------
-    
+
 r = 8 # 2^{-r} is our precision
 
 # concatenate the first r bits of each phiinv(y) to omaga represented as a string (omegastr)
 omegastr = ""
 for x,y in points:
     omegastr += float2binary(phiinv(y))[:r]
-  
+
 print "# ", omegastr
 omega = binary2float(omegastr) # convert omega to a mp real 
 theta = phi(omega) # compute theta
@@ -97,6 +97,6 @@ print "# ", theta
 # now run the model to recover, print the fitted values
 for x,y in points:
     ymodel = m(r*x,theta)
-    
+
     # these fitted values may then be plotted
     print x, y, float(ymodel) 
